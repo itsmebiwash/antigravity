21b9273cdf644c702e3c083c2c8c2e84d0ff6e98
itsmebiwash <rjbiwash@gmail.com>
2026-03-13 15:46:00 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/nebula',
+  // Ye line sabse zaroori hai proxy setup ke liye
+  assetPrefix: '/nebula',
+  // 404 fix karne ke liye trailing slash ko handle karein
+  trailingSlash: true,
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
